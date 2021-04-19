37d89bb6e918eb50beae976f9b7da9a5229ff988
= <=>
2021-04-19 03:00:28 -0500

    change github

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const path = require('path');
 
 const clips = [
   'better',
@@ -74,7 +75,7 @@ client.on('message', message => {
     message.channel.send(`\n\n_Example_ \n \`!macho yeah\`
 
 Found a bug? Open an issue on github!
-https://github.com/Tankcaster/machoBot`);
+https://github.com/SalixArctica/machoBot`);
   }
   //play random clip
   else if(args[0] == 'random'){
@@ -144,19 +145,31 @@ const play = (message, file) => {
   // Only try to join the sender's voice channel if they are in one themselves
   if (message.member.voiceChannel) {
     message.member.voiceChannel.join()
-      .then(connection => {
+    .then(connection => {
 
-	connection.on('error', console.error);
+      connection.on('error', console.error);
+  
+        console.log(path.join(__dirname, '/audio/', file + '.mp3'));
 
-        const clip = connection.playFile(__dirname + '/audio/' + file + '.mp3');
+        const clip = connection.playFile(path.join(__dirname, '/audio/', file + '.mp3'));
+
+        clip.on('error', err => {
+          console.error(err);
+        })
+
         clip.on('end', () => { //leave when clip is over
-          message.delete()
+
+          setTimeout(message => {
+            message.delete()
             .then(msg => console.log(`message deleted from ${msg.author.username}`))
             .catch(console.error);
-          message.member.voiceChannel.leave();
+            message.member.voiceChannel.leave();
+          }, 2000)
+          
         })
       })
       .catch(console.log);
+      
   } else {
     message.reply('You need to join a voice channel first brother!');
   }
